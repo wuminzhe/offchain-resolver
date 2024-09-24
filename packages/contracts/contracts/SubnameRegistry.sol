546f8b73a1af42e34b9c38c4c37ee466c775c06d
Aki Wu <wuminzhe@gmail.com>
2024-09-24 10:51:24 +0800

    update

--- packages/contracts/contracts/SubnameRegistry.sol
+++ packages/contracts/contracts/SubnameRegistry.sol
@@ -48,37 +48,8 @@ contract SubnameRegistry is ERC721, ERC721Enumerable {
         _;
     }
 
-    /**
-     * @dev Returns the length of a given string
-     *
-     * @param s The string to measure the length of
-     * @return The length of the input string
-     */
-    function strlen(string memory s) internal pure returns (uint256) {
-        uint256 len;
-        uint256 i = 0;
-        uint256 bytelength = bytes(s).length;
-        for (len = 0; i < bytelength; len++) {
-            bytes1 b = bytes(s)[i];
-            if (b < 0x80) {
-                i += 1;
-            } else if (b < 0xE0) {
-                i += 2;
-            } else if (b < 0xF0) {
-                i += 3;
-            } else if (b < 0xF8) {
-                i += 4;
-            } else if (b < 0xFC) {
-                i += 5;
-            } else {
-                i += 6;
-            }
-        }
-        return len;
-    }
-
     function valid(string memory name) public pure returns (bool) {
-        return strlen(name) >= 3;
+        return _strlen(name) >= 3;
     }
 
     function commit(bytes32 commitment) public {
@@ -182,4 +153,34 @@ contract SubnameRegistry is ERC721, ERC721Enumerable {
     {
         return super.supportsInterface(interfaceId);
     }
+
+    /**
+     * @dev Returns the length of a given string
+     *
+     * @param s The string to measure the length of
+     * @return The length of the input string
+     */
+    function _strlen(string memory s) internal pure returns (uint256) {
+        uint256 len;
+        uint256 i = 0;
+        uint256 bytelength = bytes(s).length;
+        for (len = 0; i < bytelength; len++) {
+            bytes1 b = bytes(s)[i];
+            if (b < 0x80) {
+                i += 1;
+            } else if (b < 0xE0) {
+                i += 2;
+            } else if (b < 0xF0) {
+                i += 3;
+            } else if (b < 0xF8) {
+                i += 4;
+            } else if (b < 0xFC) {
+                i += 5;
+            } else {
+                i += 6;
+            }
+        }
+        return len;
+    }
+
 }
